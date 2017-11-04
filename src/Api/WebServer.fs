ccbea95213dbcb266a4c4ba7f67a6ae6900e83dd
Anders Kofoed <anders.kfd@gmail.com>
2017-11-04 13:58:36 +0100

    Add static file server with plain index.html

--- src/Api/WebServer.fs
+++ src/Api/WebServer.fs
@@ -3,19 +3,31 @@ module Api.WebServer
 open Suave
 open Suave.Filters
 open Suave.Operators
+open System.IO
 
 let start connectionString =
     let app = 
         choose [
             GET >=> choose [
+                path "/" >=> Files.browseFileHome "index.html"
+                //pathRegex @"/(public|js|css|Images)/(.*)\.(css|png|gif|jpg|js|map)" >=> Files.browseHome
                 path "/api/wines" >=> Wines.getAllWines connectionString
             ]
             POST >=> choose [
                 path "/api/wines/" >=> Wines.addNewWine
             ]
         ]
+
+    let appPath =
+        let path = Path.Combine("..","App")
+        if Directory.Exists path 
+        then (path |> Path.GetFullPath)
+        else (path |> Path.GetFullPath)
+
+
     let config =
         { defaultConfig with 
+            homeFolder = Some appPath
             bindings = [ HttpBinding.createSimple HTTP "0.0.0.0" 8083 ]
         }
     
